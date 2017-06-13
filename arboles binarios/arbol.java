20ef79ba3949667762960100661a5c3d61d09cec
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-06-13 17:24:42 -0500

    implementacion de arboles binarios

---
+++ arboles binarios/arbol.java
@@ -0,0 +1,64 @@
+package nestle;
+
+public class arbol {
+
+    nodo_arbol raiz;
+    int can;
+
+    public arbol() {
+        raiz = null;
+        can = 0;
+    }
+
+    public void insertar(int n) {
+        nodo_arbol nuevo = new nodo_arbol(n);
+
+        if (raiz == null) {
+            raiz = nuevo;
+        } else {
+            nodo_arbol aux = raiz;
+
+            while (true) {
+                if (n > aux.dato) {
+                    if (aux.derecha == null) {
+                        aux.derecha = nuevo;
+                        return;
+                    }
+                    aux = aux.derecha;
+                } else {
+                    if (aux.izquierda == null) {
+                        aux.izquierda = nuevo;
+                        return;
+                    }
+                    aux = aux.izquierda;
+                }
+            }
+        }
+    }
+
+    public boolean buscar(int n) {
+        nodo_arbol aux = raiz;
+
+        while (aux != null) {
+            if (aux.dato == n) {
+                return true;
+            }
+
+            if (n > aux.dato) {
+                aux = aux.derecha;
+            } else {
+                aux = aux.izquierda;
+            }
+        }
+        
+        return false;
+    }
+    
+    public void mostrar(nodo_arbol aux){
+        if (aux != null) {
+            mostrar(aux.izquierda);
+            mostrar(aux.derecha);
+            System.out.println(aux.dato);
+        }
+    }
+}
---
+++ arboles binarios/nodo_arbol.java
@@ -0,0 +1,16 @@
+package nestle;
+
+public class nodo_arbol {
+    
+    int dato;
+    nodo_arbol derecha;
+    nodo_arbol izquierda;
+    
+    public nodo_arbol(int q){
+        dato = q;
+    }
+    
+    public void mostrar(){
+        System.out.println("dato= " + dato);
+    }
+}
